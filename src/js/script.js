e577723670b1d1b7abc41e3d3d80d8595f50e4a1
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-09-26 00:58:18 -0300

    primeira atualização

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+function abrirRecursos() {
+    window.open('https://intranet.kinghost.com.br/kinghost.cgi?ficha=PREENCHAAQUI');
+    window.open('https://intranet.kinghost.com.br/');
+    window.open('https://www.whois.com/whois/')
+    window.open('https://registro.br/tecnologia/ferramentas/whois/')
+    window.open('https://www.nslookup.io/')
+}
+
+function iniciar() {
+    window.open('https://rocketchat.kinghost.net/');
+    window.open('https://expert.brasil.adp.com/ipclogin/1/loginform.html?TYPE=33554433&REALMOID=06-000a1470-e058-1656-b22f-441e0bf0d04d&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-Rzp3MZOuKy1YZE74SjZ3B3L3VgJdxtvy7ol1oAjLuMC6C5YIpcVA5SeL1rGK%2beBY&TARGET=-SM-https%3a%2f%2fexpert%2ebrasil%2eadp%2ecom%2fexpert%2fv4%2ftimesheet');
+    window.open('https://owa.exchangehosting.com.br/owa/auth/logon.aspx?url=https%3a%2f%2fowa.exchangehosting.com.br%2fowa%2f&reason=0')
+    window.open('https://intranet.kinghost.com.br/')
+}
