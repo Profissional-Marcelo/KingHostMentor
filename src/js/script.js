fe338eea5d0c4fabe25360768beda977d0e0a3d6
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-10-05 00:12:53 -0300

    atualização

--- src/js/script.js
+++ src/js/script.js
@@ -1,89 +1,122 @@
-function abrirRecursos() {
-    window.open('https://intranet.kinghost.com.br/kinghost.cgi?ficha=PREENCHAAQUI');
-    window.open('https://intranet.kinghost.com.br/');
-    window.open('https://www.whois.com/whois/')
-    window.open('https://registro.br/tecnologia/ferramentas/whois/')
-    window.open('https://www.nslookup.io/')
-}
+//MENU PRINCIPAL
 
-function iniciar() {
-    window.open('https://rocketchat.kinghost.net/');
-    window.open('https://expert.brasil.adp.com/ipclogin/1/loginform.html?TYPE=33554433&REALMOID=06-000a1470-e058-1656-b22f-441e0bf0d04d&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-Rzp3MZOuKy1YZE74SjZ3B3L3VgJdxtvy7ol1oAjLuMC6C5YIpcVA5SeL1rGK%2beBY&TARGET=-SM-https%3a%2f%2fexpert%2ebrasil%2eadp%2ecom%2fexpert%2fv4%2ftimesheet');
-    window.open('https://owa.exchangehosting.com.br/owa/auth/logon.aspx?url=https%3a%2f%2fowa.exchangehosting.com.br%2fowa%2f&reason=0')
-    window.open('https://intranet.kinghost.com.br/')
-}
+$("#btnDominio").click(()=>{
+    $("#menuEmail").hide();
+    $("#menuIntranet").hide();
+    $("#menuEncurtador").hide();
+    $("#menuRede").hide();
+    $('#menuVideos').hide();
+    $("#menuDominio").toggle();
+});
 
-    let botao = window.document.getElementById('dominio-button');
-    botao.addEventListener('click', abrirDominio)
+$("#btnEmail").click(()=>{
+    $("#menuDominio").hide();
+    $("#menuIntranet").hide();
+    $("#menuEncurtador").hide();
+    $("#menuRede").hide();
+    $('#menuVideos').hide();
+    $("#menuEmail").toggle();
+});
 
-    function abrirDominio(){
-        fecharOutrosMenus('dominio-resmenu');
-        let menu = window.document.getElementById('dominio-resmenu');
-        if (menu.style.display === 'inline-block') {
-            menu.style.display = 'none'; // Fecha a barra de menu
-        } else {
-            menu.style.display = 'inline-block'; // Abre a barra de menu
-        }
-        
-    }
+$("#btnIntranet").click(()=>{
+    $("#menuEmail").hide();
+    $("#menuDominio").hide();
+    $("#menuEncurtador").hide();
+    $("#menuRede").hide();
+    $('#menuVideos').hide();
+    $("#menuIntranet").toggle();
+});
 
-    let emailButton = document.getElementById('email-button');
-    emailButton.addEventListener('click', abrirEmail);
+$("#btnEncurtador").click(()=>{
+    $("#menuEmail").hide();
+    $("#menuDominio").hide();
+    $("#menuIntranet").hide();
+    $("#menuRede").hide();
+    $('#menuVideos').hide();
+    $("#menuEncurtador").toggle();
+});
 
-    function abrirEmail() {
-        fecharOutrosMenus('email-resmenu'); // Fecha outros menus
-        let menu = document.getElementById('email-resmenu');
-        if (menu.style.display === 'inline-block') {
-            menu.style.display = 'none'; // Fecha o menu
-        } else {
-            menu.style.display = 'inline-block'; // Abre o menu
-        }
-    }
-    
-    let intranetButton = document.getElementById('intranet-button');
-    intranetButton.addEventListener('click', abrirIntranet);
+$("#btnRede").click(()=>{
+    $("#menuEmail").hide();
+    $("#menuDominio").hide();
+    $("#menuIntranet").hide();
+    $("#menuEncurtador").hide();
+    $('#menuVideos').hide();
+    $("#menuRede").toggle();
+});
 
-    function abrirIntranet() {
-        fecharOutrosMenus('intranet-resmenu'); // Fecha outros menus
-        let menu = document.getElementById('intranet-resmenu');
-        if (menu.style.display === 'inline-block') {
-            menu.style.display = 'none'; // Fecha o menu
-        } else {
-            menu.style.display = 'inline-block'; // Abre o menu
-        }
-    }
+$("#btnVideos").click(()=>{
+    $("#menuEmail").hide();
+    $('#menuDominio').hide();
+    $('#menuIntranet').hide();
+    $('#menuEncurtador').hide();
+    $('#menuRede').hide();
+    $('#menuVideos').toggle();
+});
 
-    let encurtadorButton = document.getElementById('encurtador-button');
-    encurtadorButton.addEventListener('click', abrirEncurtador);
+//SUBMENU
 
-    function abrirEncurtador() {
-        fecharOutrosMenus('encurtador-resmenu'); // Fecha outros menus
-        let menu = document.getElementById('encurtador-resmenu');
-        if (menu.style.display === 'inline-block') {
-            menu.style.display = 'none'; // Fecha o menu
-        } else {
-            menu.style.display = 'inline-block'; // Abre o menu
-        }
-    }
+//DOMINIO
+$("#btnWhois").click(()=>{
+    $("#menuRegistro").hide();
+    $("#menuDns").hide();
+    $("#menuWhois").toggle();
+});
 
-    let redeButton = document.getElementById('rede-button');
-    encurtadorButton.addEventListener('click', abrirRede);
+$("#btnDns").click(()=>{
+    $("#menuRegistro").hide();
+    $("#menuWhois").hide();
+    $("#menuDns").toggle();
+});
 
-    function abrirRede() {
-        fecharOutrosMenus('rede-resmenu'); // Fecha outros menus
-        let menu = document.getElementById('rede-resmenu');
-        if (menu.style.display === 'inline-block') {
-            menu.style.display = 'none'; // Fecha o menu
-        } else {
-            menu.style.display = 'inline-block'; // Abre o menu
-        }
-    }
+$("#btnRegistro").click(()=>{
+    $("#menuDns").hide();
+    $("#menuWhois").hide();
+    $("#menuRegistro").toggle();
+});
 
-    function fecharOutrosMenus(excecaoMenuId) {
-        let menus = document.querySelectorAll('.rpmenu');
-        menus.forEach(function (menu) {
-            if (menu.id !== excecaoMenuId) {
-                menu.style.display = 'none'; // Fecha o menu
-            }
-        });
-    }
+//E-MAIL
+$("#btnBlock").click(()=>{
+    $("#menuBol").hide();
+    $("#menuGmail").hide();
+    $("#menuOutlook").hide();
+    $("#menuYahoo").hide();
+    $("#menuBlock").toggle();
+});
+
+$("#btnBol").click(()=>{
+    $("#menuBlock").hide();
+    $("#menuGmail").hide();
+    $("#menuOutlook").hide();
+    $("#menuYahoo").hide();
+    $("#menuBol").toggle();
+});
+
+$("#btnGmail").click(()=>{
+    $("#menuBlock").hide();
+    $("#menuBol").hide();
+    $("#menuOutlook").hide();
+    $("#menuYahoo").hide();
+    $("#menuGmail").toggle();
+});
+
+$("#btnOutlook").click(()=>{
+    $("#menuBlock").hide();
+    $("#menuBol").hide();
+    $("#menuGmail").hide();
+    $("#menuYahoo").hide();
+    $("#menuOutlook").toggle();
+});
+
+$("#btnYahoo").click(()=>{
+    $("#menuBlock").hide();
+    $("#menuBol").hide();
+    $("#menuGmail").hide();
+    $("#menuOutlook").hide();
+    $("#menuYahoo").toggle();
+});
+
+//Youtube
+$("#btnVideoUm").click(()=>{
+    $("#menuVideoUm").toggle();
+});
