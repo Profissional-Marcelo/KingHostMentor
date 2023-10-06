7b745a11b30e10dc93449cedc385b2036774602a
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-10-05 23:05:26 -0300

    Atualização Host Mentor

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
+//ENCURTADORES
+function abrirRecursos() {
+    window.open('https://intranet.kinghost.com.br/kinghost.cgi?ficha=PREENCHAAQUI');
+    window.open('https://intranet.kinghost.com.br/');
+    window.open('https://www.whois.com/whois/')
+    window.open('https://registro.br/tecnologia/ferramentas/whois/')
+    window.open('https://www.nslookup.io/')
+}
+function iniciar() {
+    window.open('https://rocketchat.kinghost.net/');
+    window.open('https://expert.brasil.adp.com/ipclogin/1/loginform.html?TYPE=33554433&REALMOID=06-000a1470-e058-1656-b22f-441e0bf0d04d&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-Rzp3MZOuKy1YZE74SjZ3B3L3VgJdxtvy7ol1oAjLuMC6C5YIpcVA5SeL1rGK%2beBY&TARGET=-SM-https%3a%2f%2fexpert%2ebrasil%2eadp%2ecom%2fexpert%2fv4%2ftimesheet');
+    window.open('https://owa.exchangehosting.com.br/owa/auth/logon.aspx?url=https%3a%2f%2fowa.exchangehosting.com.br%2fowa%2f&reason=0')
+    window.open('https://intranet.kinghost.com.br/')
+}
+
+
 //MENU PRINCIPAL
 
 $("#btnDominio").click(()=>{
@@ -54,68 +70,42 @@ $("#btnVideos").click(()=>{
     $('#menuVideos').toggle();
 });
 
+
 //SUBMENU
 
 //DOMINIO
 $("#btnWhois").click(()=>{
-    $("#menuRegistro").hide();
     $("#menuDns").hide();
     $("#menuWhois").toggle();
 });
 
 $("#btnDns").click(()=>{
-    $("#menuRegistro").hide();
     $("#menuWhois").hide();
     $("#menuDns").toggle();
 });
 
-$("#btnRegistro").click(()=>{
-    $("#menuDns").hide();
-    $("#menuWhois").hide();
-    $("#menuRegistro").toggle();
-});
-
 //E-MAIL
 $("#btnBlock").click(()=>{
-    $("#menuBol").hide();
-    $("#menuGmail").hide();
-    $("#menuOutlook").hide();
-    $("#menuYahoo").hide();
+    $("#menuMailsTest").hide();
     $("#menuBlock").toggle();
 });
 
-$("#btnBol").click(()=>{
+$("#btnMailsTest").click(()=>{
     $("#menuBlock").hide();
-    $("#menuGmail").hide();
-    $("#menuOutlook").hide();
-    $("#menuYahoo").hide();
-    $("#menuBol").toggle();
+    $("#menuMailsTest").toggle();
 });
 
-$("#btnGmail").click(()=>{
-    $("#menuBlock").hide();
-    $("#menuBol").hide();
-    $("#menuOutlook").hide();
-    $("#menuYahoo").hide();
-    $("#menuGmail").toggle();
+
+//Rede
+$("#btnMeuIp").click(()=>{
+    $("#menuProxy").hide();
+    $("#menuMeuIp").toggle();
 });
 
-$("#btnOutlook").click(()=>{
-    $("#menuBlock").hide();
-    $("#menuBol").hide();
-    $("#menuGmail").hide();
-    $("#menuYahoo").hide();
-    $("#menuOutlook").toggle();
-});
-
-$("#btnYahoo").click(()=>{
-    $("#menuBlock").hide();
-    $("#menuBol").hide();
-    $("#menuGmail").hide();
-    $("#menuOutlook").hide();
-    $("#menuYahoo").toggle();
-});
-
+$("#btnProxy").click(()=>{
+    $("#menuMeuIp").hide();
+    $("#menuProxy").toggle();
+})
 //Youtube
 $("#btnVideoUm").click(()=>{
     $("#menuVideoUm").toggle();
