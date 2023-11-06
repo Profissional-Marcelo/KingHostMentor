3c9519d81002d2a156e4268070d90dcd8323fb7e
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-11-05 21:16:13 -0300

    app

---
+++ Assets/js/quiz.js
@@ -0,0 +1,14 @@
+const pergunta = document.getElementById('pergunta')
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+
+function carregar(){
+    const aleatorio = random(1, 5)
+
+
+}
